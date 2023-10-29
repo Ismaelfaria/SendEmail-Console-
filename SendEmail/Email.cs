6a92f060f16cffee4595e183f0ba39518b9bdb87
limaismael8901 <limaismael8901@gmail.com>
2023-10-29 07:08:45 -0300

    Version 29.10

--- SendEmail/Email.cs
+++ SendEmail/Email.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Mail;
-using System.Net.Mime;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace SendEmail
 {
